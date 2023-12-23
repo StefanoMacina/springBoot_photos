e2d2454fe0ed5e4d1cce3dbf204d0d2f9bc89363
Stefano <s.macinaleone@gmail.com>
2023-12-23 14:48:38 +0100

    add : base project

---
+++ src/main/java/com/example/MyPhotos/controllers/HomeController.java
@@ -0,0 +1,17 @@
+package com.example.MyPhotos.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@Controller
+public class HomeController {
+
+    @RequestMapping("/")
+    public String homePage(){
+        return "index";
+    }
+
+}
